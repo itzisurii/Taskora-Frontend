b0e6375065b68eb2858efea87e7ba229582f8915
itzisurii <itzIsurika08@gmail.com>
2026-02-21 13:07:01 +0530

    feat: add LoginInComponent with HTML, CSS, and spec files for user login functionality

---
+++ src/app/pages/login-in/login-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-in',
+  imports: [],
+  templateUrl: './login-in.component.html',
+  styleUrl: './login-in.component.css'
+})
+export class LoginInComponent {
+
+}

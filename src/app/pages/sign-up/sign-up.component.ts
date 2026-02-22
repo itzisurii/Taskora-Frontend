ef4e4c6b185c2f7228468f74b488521ec2e81eb5
itzisurii <itzIsurika08@gmail.com>
2026-02-22 09:26:13 +0530

    feat: update sign-up component with new layout, styles, and routing functionality

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MainPageComponent } from '../main-page/main-page.component';
 
 @Component({
   selector: 'app-sign-up',
+  standalone: true,
   imports: [],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  constructor(private router: Router){}
 
+  goToTask(target: 'login' | 'mainPage1' | 'mainPage2' | 'mainPage3'){
+    const routes = {
+      login: '/login',
+      mainPage1: '/mainPage1',
+      mainPage2: '/mainPage2',
+      mainPage3: '/mainPage3'
+    }
+
+    this.router.navigate([routes[target]]);
+  }
+  
 }

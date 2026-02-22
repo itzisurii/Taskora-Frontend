2e487d0240b3be6e938fdb681a1e712f5b983cd1
itzisurii <itzIsurika08@gmail.com>
2026-02-22 12:41:44 +0530

    feat: implement navigation functionality in MainPageComponent

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+  constructor(private router: Router){}
 
+  goToTask(target: 'mainPage2' | 'dashboard'){
+    const routes = {
+      mainPage2: '/mainPage2',
+      dashboard: '/dashboard'
+    }
+    this.router.navigate([routes[target]])
+  }
 }

acbc396fcd8ea1b2c2e3316e76bbedfb47207db8
itzisurii <itzIsurika08@gmail.com>
2026-02-23 02:29:19 +0530

    feat: add NewTaskComponent to HomeComponent imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,10 +11,11 @@ import { TaskToolbarComponent } from "../../components/task-toolbar/task-toolbar
 import { MainPageComponent } from "../main-page/main-page.component";
 import { MainPage2Component } from "../main-page2/main-page2.component";
 import { MainPage3Component } from "../main-page3/main-page3.component";
+import { NewTaskComponent } from "../../components/new-task/new-task.component";
 
 @Component({
   selector: 'app-home',
-  imports: [LoginInComponent, SignUpComponent, NavBarComponent, RouterOutlet, FooterComponent, NameContainerComponent, TaskListComponent, TaskCardComponent, TaskToolbarComponent, MainPageComponent, MainPage2Component, MainPage3Component],
+  imports: [LoginInComponent, SignUpComponent, NavBarComponent, RouterOutlet, FooterComponent, NameContainerComponent, TaskListComponent, TaskCardComponent, TaskToolbarComponent, MainPageComponent, MainPage2Component, MainPage3Component, NewTaskComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

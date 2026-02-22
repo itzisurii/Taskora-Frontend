9c6f0e7598cc7a53402bc8419ad0ebe29759896e
itzisurii <itzIsurika08@gmail.com>
2026-02-23 02:29:12 +0530

    feat: create NewTaskComponent with initial structure and routing

---
+++ src/app/components/new-task/new-task.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { routes } from '../../app.routes';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+
+}

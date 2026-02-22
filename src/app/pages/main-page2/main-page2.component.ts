c79118b3f69a232dd1137c04375539baf6e4320c
itzisurii <itzIsurika08@gmail.com>
2026-02-22 09:25:20 +0530

    feat: add MainPage2Component with HTML, CSS, TypeScript, and spec files for main page 2 functionality

---
+++ src/app/pages/main-page2/main-page2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page2',
+  imports: [],
+  templateUrl: './main-page2.component.html',
+  styleUrl: './main-page2.component.css'
+})
+export class MainPage2Component {
+
+}
